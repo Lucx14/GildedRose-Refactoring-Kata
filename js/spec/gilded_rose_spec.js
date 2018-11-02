d95ed23c2758043d59db978440a80fc557813aab
Lucien Najev <luciennajev@hotmail.com>
2018-11-02 12:52:12 +0000

    Testing: written out 4 tests for Aged Brie

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -1,6 +1,6 @@
 describe('Gilded Rose', () => {
 
-  describe('Item constructor', () => {
+  describe('Item class', () => {
 
     let gildedRose;
 
@@ -20,18 +20,52 @@ describe('Gilded Rose', () => {
       expect(gildedRose.items[0].sellIn).toEqual(0);
     });
   });
-  
 
-  describe('Aged Brie', () => {
-    it('Aged Brie item name', () => {
-      const gildedRose = new Shop([ new Item('Aged Brie', 2, 0) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].name).toEqual('Aged Brie');
+  describe('Shop class', () => {
+
+    describe('updateQuality function', () => {
+
+      describe('Aged Brie', () => {
+        let gildedRose;
+
+        beforeEach(() => {
+          gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
+        });
+
+        it('SellIn days count down', () => {
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          expect(gildedRose.items[0].sellIn).toEqual(-3);
+        });
+
+        it('Quality increases by 1 for each update before sellIn date', () => {
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          expect(gildedRose.items[0].quality).toEqual(2);
+        });
+        it('Quality increments by 2 after the sellIn date', () => {
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          expect(gildedRose.items[0].quality).toEqual(8);
+        });
+        it('Quality has max limit of 50', () => {
+          let times = 50;
+          for (let i=0; i < times; i++) {
+            gildedRose.updateQuality();
+          }
+          expect(gildedRose.items[0].quality).toEqual(50);
+
+        });
+      }); 
+    
     });
-    it('Aged Brie item sellIn number of days', () => {
-      const gildedRose = new Shop([ new Item('Aged Brie', 2, 0) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].name).toEqual('Aged Brie');
-    });
-  }); 
+
+  });
+  
 });
