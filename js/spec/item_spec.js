242d573ca43e4ad52cec5ce35695d062f8803f38
Lucien Najev <luciennajev@hotmail.com>
2018-11-02 18:30:21 +0000

    feature: added a subclass for normal products to the Item superclass

--- js/spec/item_spec.js
+++ js/spec/item_spec.js
@@ -1,20 +1,53 @@
-describe('Item class', () => {
+describe('Item', () => {
 
-    let item;
+  let item;
 
-    beforeEach(() => {
-      item = new Item("foo", 0, 0);
-    });
-
-    it('Item name', () => {
-      expect(item.name).toEqual("foo");
-    });
-  
-    it('Item quality', () => {
-      expect(item.quality).toEqual(0);
-    });
-
-    it('Item sellIn', () => {
-      expect(item.sellIn).toEqual(0);
-    });
+  beforeEach(() => {
+    item = new Item("foo", 0, 0);
   });
+
+  it('Item name', () => {
+    expect(item.name).toEqual("foo");
+  });
+
+  it('Item quality', () => {
+    expect(item.quality).toEqual(0);
+  });
+
+  it('Item sellIn', () => {
+    expect(item.sellIn).toEqual(0);
+  });
+});
+
+describe('Normal', () => {
+  let item;
+
+  beforeEach(() => {
+    item = new Normal("normal product", 2, 4);
+  });
+
+  it('sellIn days count down by 1 each update', () => {
+    item.update();
+    expect(item.sellIn).toEqual(1);
+  });
+
+  it('quality -1 each update if sellIn > 0', () => {
+    item.update();
+    expect(item.quality).toEqual(3);
+  });
+
+  it('quality -2 each update if sellIn < 0', () => {
+    item.update();
+    item.update();
+    item.update();
+    expect(item.quality).toEqual(0);
+  });
+
+  it('quality cannot be < 0', () => {
+    item.update();
+    item.update();
+    item.update();
+    item.update();
+    expect(item.quality).toEqual(0);
+  });
+});
--- js/src/item.js
+++ js/src/item.js
@@ -1,7 +1,22 @@
 class Item {
-    constructor(name, sellIn, quality){
-      this.name = name;
-      this.sellIn = sellIn;
-      this.quality = quality;
-    }
+  constructor(name, sellIn, quality){
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
   }
+}
+
+
+class Normal extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality);
+  }
+
+  update() {
+    this.sellIn -= 1;
+    this.sellIn < 0 ? this.quality -= 2 : this.quality -= 1;
+    this.quality <= 0 ? this.quality = 0 : null;
+  }
+}
+
+
