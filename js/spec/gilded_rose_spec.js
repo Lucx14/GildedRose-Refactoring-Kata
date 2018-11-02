170a7b295476e14313aa7a13010bd6121ffb0185
Lucien Najev <luciennajev@hotmail.com>
2018-11-02 13:50:15 +0000

    testing: added tests for sulfuras

--- js/spec/gilded_rose_spec.js
+++ js/spec/gilded_rose_spec.js
@@ -107,6 +107,25 @@ describe('Gilded Rose', () => {
           expect(gildedRose2.items[0].quality).toEqual(50);
         });
       });
+
+      describe('Sulfuras', () => {
+        let gildedRose;
+
+        beforeEach(() => {
+          gildedRose = new Shop([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
+        });
+
+        it('has a constant quality of 80', () => {
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          expect(gildedRose.items[0].quality).toEqual(80);
+        });
+        it('never has to be sold (sellIn doesnt change)', () => {
+          gildedRose.updateQuality();
+          gildedRose.updateQuality();
+          expect(gildedRose.items[0].sellIn).toEqual(0);
+        });
+      });
     
     });
 
