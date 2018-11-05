7a98f512f79c45fd75d1bee2dea0ed50b4570990
Lucien Najev <luciennajev@hotmail.com>
2018-11-05 10:04:52 +0000

    removed dependency in shop tests on item, also added final unt test for update quality

--- js/spec/shop_spec.js
+++ js/spec/shop_spec.js
@@ -2,14 +2,14 @@ describe('Shop class: Unit tests', () => {
 
   describe('getItemClass', () => {
     it('will get the correct key from the itemClasses matrix', () => {
-      let gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
+      let gildedRose = new Shop([{ name: "Aged Brie", sellIn: 2, quality: 0 }]);
       expect(gildedRose.getItemClass(gildedRose.items[0])).toEqual('Aged Brie');
     });
   });
 
   describe('categorize', () => {
     it('creates a new constructor variable containing categorized items', () => {
-      let gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
+      let gildedRose = new Shop([{ name: "Aged Brie", sellIn: 2, quality: 0 }]);
       gildedRose.categorize();
       expect(gildedRose.categorizedItems[0]).toEqual(jasmine.any(AgedBrie));
     });
@@ -17,20 +17,36 @@ describe('Shop class: Unit tests', () => {
 
   describe('runStockUpdate', () => {
     it('updates the item objects sellIn and quality', () => {
-      let gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
+      let gildedRose = new Shop([{ name: "Aged Brie", sellIn: 2, quality: 0 }]);
       gildedRose.categorize();
       gildedRose.runStockUpdate();
       expect(gildedRose.items[0].quality).toEqual(0);
     });
   });
 
-  // describe('update quality', () => {
-  //   let gildedRose = new Shop([ new Item("Aged Brie", 2, 0) ]);
-  //     gildedRose.categorize();
-  //     gildedRose.runStockUpdate();
-  //     gildedRose.updateQuality();
-  //     expect(item.update()).toHaveBeenCalled();
-  // });
+  describe('update quality', () => {
+    it('updates the quality and sellin dates', () => {
+      let item = { name: 'Aged Brie', sellIn: 2, quality: 0 }
+      let gildedRose = new Shop([item]);
+      gildedRose.updateQuality();
+      expect(gildedRose.items[0].quality).toEqual(1);
+    });
+  });
 
 });
+
+
+// private method example to think about....
+// const _resetTable = Symbol('resetTable');
+
+// resetTable(tableName) {
+//   this.replaceTable(tableName, this.initialData(tableName))
+// }
+
+
+// [_resetTable](tableName) {
+//   this[_replaceTable](tableName, this[_initialData](tableName))
+// }
+
+
   
